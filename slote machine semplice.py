ab6675d634e5a7356b52771d1ba4b04ecd4a6418
Kirstian_Git <108211729+Feranko@users.noreply.github.com>
2023-03-08 22:11:41 +0100

    Update slote machine semplice.py

--- slote machine semplice.py
+++ slote machine semplice.py
@@ -1,35 +1,27 @@
 from random import randint
-from time import sleep
-import pyttsx3
-import keyboard
-
-print("1euros=5spin")
-a = int(input())
-print(f"you have:{a*5}spins ")
-
-def frankorompipalle():
-    crediti = a * 5
-    for r in range(crediti):
-        win = 0
-        for b in range(5):
-            i = [randint(1, 3)]
-            j = [randint(1, 3)]
-            k = [randint(1, 3)]
-            print(i, j, k,)
-        if (i[0]==j[0]==k[0]):
-            print("hia vinto")
-            win += 1
+def spin():
+    ingresso = int(input("int the number of spin : "))
+    win : int = 0
+    lose : int = 0
+    a,b,c=0,0,0
+    for d in range(ingresso):
+        print(f"-----------------\nspin number : {d}")
+        for e in range(5):
+            a=randint(1,3)
+            b=randint(1,3)
+            c=randint(1,3)
+            print(f"{a},{b},{c}")
+        if e==4:
+            if a==b and b==c and a==c:
+                print("you won \n")
+                win += 1
+            else:
+                print("you lose")
+                lose += 1
         else:
-            print("hai perso")
-            crediti -= 1
-        print("\n")
-        print(crediti)
-        if crediti == 0:
-            print("you finished your spin")
-            print(f"you win at {win} turn")
-
-print("press alt to play\n")
-while True:
-    if keyboard.is_pressed("alt"):
-        frankorompipalle()
-print("\n")
+                pass
+    print(f"you won for : {win}, you lose for :{lose}")
+def main():
+    spin()
+if __name__=="__main__":
+    main()
