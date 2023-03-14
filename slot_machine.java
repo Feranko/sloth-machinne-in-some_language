9be019cfdfe901a41414ee0f5634f1f9d60156ba
Kirstian_Git <108211729+Feranko@users.noreply.github.com>
2023-03-14 16:08:23 +0100

    Update slot_machine.java

--- slot_machine.java
+++ slot_machine.java
@@ -1,31 +1,38 @@
-import java.util.*;
-public class slot_machine {
+import java.util.Random;
+import java.util.Scanner;
+public class sloth_machie {
     public static void main(String[] args){
+         while(true){
+            System.out.println("int the number of spin : ");
+            sloth();
+         }
+    }
+    public static void sloth(){
+        int a,b,c = 0;
+        int win=0;
+	int lose=0;
         Random rand = new Random();
-        System.out.println("int the number of the spin you wnat to do : ");
-        Scanner scan = new Scanner(System.in);
-        int limite = scan.nextInt();
-        int vincite = 0;
-        int a,b,c=0;
-        for(int j=0;j<=limite;++j){
-            for(int i=0;i<=5;++i){
-                a=rand.nextInt(9);
-                b=rand.nextInt(9);
-                c=rand.nextInt(9);
-                System.out.println(a+"-"+b+"-"+c);
-                if(i>=5){
-                    if(a==b && b==c){
-                        System.out.println("you won");
-                        vincite += 1; 
-                    }
-                    else{
-                        System.out.println("you lose");
-                    }
-                }
-            }
-            if(j>=limite){
-                System.out.println("you won for : "+vincite+" times");
-            }
-        }
+        Scanner ingresso = new Scanner(System.in);
+	int limite = ingresso.nextInt();
+        for(int d=0;d<limite;++d){
+		System.out.println("----"+(d+1)+"----");
+            for(int e=0;e<=5;++e){
+                a=rand.nextInt(4);
+                b=rand.nextInt(4);
+                c=rand.nextInt(4);
+                System.out.println("  "+a+","+b+","+c);
+		if(e>=5){
+	 		if(a==b && b==c){
+				System.out.println("you won");
+				win += 1; 
+				}
+			else{
+				System.out.println("you lose");
+				lose += 1;
+				}
+        		}
+    		}
+	}
+	System.out.println("you won for : "+win+" you lose for : "+lose);
     }
 }
