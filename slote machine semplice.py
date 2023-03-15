13264d3d4741ff93477294d7aa7f6dec73a1d9e0
Kirstian_Git <108211729+Feranko@users.noreply.github.com>
2023-03-15 08:51:12 +0100

    Update slote machine semplice.py

--- slote machine semplice.py
+++ slote machine semplice.py
@@ -1,27 +1,35 @@
-from random import randint
+import random
+import numpy as np
 def spin():
-    ingresso = int(input("int the number of spin : "))
-    win : int = 0
-    lose : int = 0
-    a,b,c=0,0,0
+    a = np.array(["0","0","0","0","0"])
+    b = np.array(["0","0","0","0","0"])
+    c = np.array(["0","0","0","0","0"])
+    win=0
+    lose=0
+    ingresso = int(input())
     for d in range(ingresso):
-        print(f"-----------------\nspin number : {d}")
+        print(f"\n----{d+1}----")
         for e in range(5):
-            a=randint(1,3)
-            b=randint(1,3)
-            c=randint(1,3)
-            print(f"{a},{b},{c}")
-        if e==4:
-            if a==b and b==c and a==c:
-                print("you won \n")
-                win += 1
+            a[e] = random.randint(1,3)
+            b[e] = random.randint(1,3)
+            c[e] = random.randint(1,3)
+            print(f"  {a[e]},{b[e]},{c[e]}")
+            
+        if a[4]==b[4]:
+            if b[4]==c[4]:
+                print("you win");
+                win=win+1
             else:
                 print("you lose")
-                lose += 1
+                lose=lose+1
         else:
-                pass
-    print(f"you won for : {win}, you lose for :{lose}")
+                print("you lose")
+                lose=lose+1
+    print(f"you won for :{win}, you lose for :{lose}")
 def main():
-    spin()
+    while True:
+        print("\n int the number of spin \n")
+        spin()
+ 
 if __name__=="__main__":
     main()
