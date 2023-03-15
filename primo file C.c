7ed4ac472425007b5f72cdee709195c8c4f7a123
Kirstian_Git <108211729+Feranko@users.noreply.github.com>
2023-03-15 22:39:48 +0100

    Update primo file C.c

--- primo file C.c
+++ primo file C.c
@@ -1,40 +1,37 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <conio.h>
-
-int main(){
-	int i;
-	int j;
-	int a[4],b[4],c[4];
-	int k=0;
+#include<stdio.h>
+#include<stdlib.h>
+#define N 5
+void spin(){
+	int ingresso = 0;
+	scanf("%d",&ingresso);
+	int a[N],b[N],c[N];
 	int win=0;
-	
-	printf("quanti punti ti giochi? ");
-	scanf("%d", &j);
-	
-	printf("a b c\n");
-	printf("-----\n");
-	
-	while (k <= j-1){
-	printf("spin n'=%d\n",k+1);
-	for(i=0;i<5;i++){
-		a[i]=1+rand()%3;
-		b[i]=1+rand()%3;
-		c[i]=1+rand()%3;
-		printf("%d ",a[i]);
-		printf("%d ",b[i]);
-		printf("%d \n",c[i]);
+	int lose=0;
+	for(int d=0;d<ingresso;d++){
+		printf("\n----%d---\n",(d+1));
+		for(int e=0;e<N;e++){
+			a[e]=1+(rand()%3);
+			b[e]=1+(rand()%3);
+			c[e]=1+(rand()%3);
+			printf("  %d,%d,%d\n",a[e],b[e],c[e]);	
+		}
+		if(a[4]==b[4] && b[4]==c[4]){
+			printf("you win\n");
+			win++;
+		}
+		else{
+			printf("you lose\n");
+			lose++;
+		}
 	}
-	if (a[4] == b[4] == c[4]){
-		printf("hai vinto\n\n");
-		win + 1;
-	}
-	else{
-		printf("hai perso\n\n");
-	}
-	/*if (k == j){
-		printf("you won: %d",win);}*/
-	k++;
+	printf("\nyou won for :%d , you lose for :%d \n",win,lose);
+	win=0;
+	lose=0;
+}
+int main(){
+	while(1){
+		printf("int the number of spin : ");
+		spin();
 	}
 	return 0;
-	}
+}
